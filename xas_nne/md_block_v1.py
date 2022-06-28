6c969fc0e2ab4f7357a239ed339c5929aab51c97
Matthew Carbone <x94carbone@gmail.com>
2022-06-28 09:24:07 -0400

    Matt updates

--- xas_nne/md_block_v1.py
+++ xas_nne/md_block_v1.py
@@ -10,48 +10,60 @@ file_ext = '.xyz'
 file_name = 'benzene'
 
 
-def split_file_on_blocks (file_path):
-    input_file =open(file_path, 'r')
-    block_separator = input_file.readline().rstrip()
-    lines= input_file.readlines()
-    block_id = ''
-    blocks = []
-    block_array =[]
-    block_num = 0
-    block_line =0
+def split_file_on_blocks(file_path):
 
-    for line in lines:
-        line_txt =line.rstrip()
-        if(line_txt==block_separator):
-            block_line = 1
-            if(block_num !=0):
-                blocks.append(block_array)
-                block_array =[]
-                block_num +=1
+    # Context manager is better practice for this
+    with open(file_path, "r") as input_file:
+
+        lines = input_file.readlines()
+
+        # Block separator is e.g. 12 for Benzene
+        # Note you don't need the extra ``readline`` operation since you've
+        # already loaded all of the file's lines into memory
+        block_separator = lines[0]
+
+        # --- Matt left off here ---
+        block_id = ''
+        blocks = []
+        block_array =[]
+        block_num = 0
+        block_line =0
+
+        for line in lines:
+            line_txt =line.rstrip()
+            if(line_txt==block_separator):
+                block_line = 1
+                if(block_num !=0):
+                    blocks.append(block_array)
+                    block_array =[]
+                    block_num +=1
+                else:
+                    block_num +=1
+            elif(block_line ==2):
+                block_id = line.rstrip()
+                print(block_id)
             else:
-                block_num +=1
-        elif(block_line ==2):
-            block_id = line.rstrip()
-            print(block_id)
-        else:
-            line_array=[block_id]
-            #line_array.extend(line.rstrip().split())
-            line_array.extend(line.rstrip().split()[0:4])
-            block_array.append(line_array)
+                line_array=[block_id]
+                #line_array.extend(line.rstrip().split())
+                line_array.extend(line.rstrip().split()[0:4])
+                block_array.append(line_array)
 
-        block_line +=1
+            block_line +=1
 
-    input_file.close()    
     return blocks
 
-file_path = dir_path + file_name + file_ext
-blocks = split_file_on_blocks(file_path)
 
-#line_array.extend(line.rstrip().split()[0:4])
+# This lets us input ``md_block_v1`` without actually running this code
+if __name__ == '__main__':
 
-print(len(blocks))
-print(len(blocks[10112]))
-print(blocks[10112])
+    file_path = dir_path + file_name + file_ext
+    blocks = split_file_on_blocks(file_path)
+
+    # line_array.extend(line.rstrip().split()[0:4])
+
+    print(len(blocks))
+    print(len(blocks[10112]))
+    print(blocks[10112])
 
 
 

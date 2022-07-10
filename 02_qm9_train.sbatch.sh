b09e1d83545e0df16751111c472d04d980c946b1
Matthew Carbone <x94carbone@gmail.com>
2022-07-10 16:09:37 -0400

    Update training scripts

---
+++ 02_qm9_train.sbatch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#SBATCH -A mlg-core
+#SBATCH -p volta
+#SBATCH -t 24:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 3
+#SBATCH --gres=gpu:1
+#SBATCH --output=Logs/job_data/test_gpu_%A.out
+#SBATCH --error=Logs/job_data/test_gpu_%A.err
+#SBATCH --job-name=ml
+
+module load gcc/8.3.0
+module load openmpi/4.0.2-gcc-8.3.0-cuda10.1
+module load pytorch/1.5.1
+module load cuda/10.2
+source env/bin/activate
+
+which python3
+python3 02_qm9_train.py "$@"
